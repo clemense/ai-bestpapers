911966ddee3f2ee67a6872d536ac570f3d3b442e
Clemens Eppner <ceppner@nvidia.com>
2021-01-31 14:35:23 -0800

    adding most cited papers

--- utils/mostcited.py
+++ utils/mostcited.py
@@ -4,6 +4,7 @@
 # Sort and Clean award data.
 # It writes to `sorted_data.yml` and `cleaned_data.yml`, copy those to the papers.yml after screening.
 
+import random
 import itertools
 import yaml
 from datetime import date, datetime
@@ -53,9 +54,8 @@ def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
     return yaml.dump(data, stream, OrderedDumper, **kwds)
 
 
-def format_author_list_old(authors):
+def _format_author_list(authors):
     add_dot = []
-    print(authors)
     for auth in authors:
         name_parts = auth.split()
         if len(name_parts[0]) == 1 or (
@@ -65,13 +65,32 @@ def format_author_list_old(authors):
         else:
             new_auth = auth
         add_dot.append(new_auth)
-    print(add_dot)
     return add_dot
 
 
-def format_author_list(authors):
+def _format_author_string(authors, abbreviate_first_name=True):
     res = authors.split(" and ")
-    return [" ".join(r.split(", ")[::-1]) for r in res]
+    tmp = []
+    for r in res:
+        name = r.split(", ")[::-1]
+        if len(name) > 2 and len(name[1]) == 1:
+            name[1] += "."
+
+        if abbreviate_first_name:
+            name[0] = name[0][0] + "."
+
+        tmp.append(" ".join(name))
+
+    return tmp
+
+
+def format_author_list(authors, abbreviate_first_name=True):
+    if isinstance(authors, list):
+        return _format_author_list(authors)
+    else:
+        return _format_author_string(
+            authors, abbreviate_first_name=abbreviate_first_name
+        )
 
 
 def date_from_string(s):
@@ -84,7 +103,7 @@ def date_from_string(s):
     return datetime.strptime(month_day + " " + year, "%B %d %Y")
 
 
-def query_generator(search_term, venue, year):
+def query_generator(search_term, venue, year, num_results, pg, grace_period=0):
     google_search_query = search_term
     query_year = year
 
@@ -94,16 +113,35 @@ def query_generator(search_term, venue, year):
         elif year == 2020:
             query_year = 2021
 
-    return scholarly.search_pubs(
-        google_search_query,
-        year_low=query_year,
-        year_high=query_year,
-        patents=False,
-    )
+    while True:
+        try:
+            search_query = scholarly.search_pubs(
+                google_search_query,
+                year_low=query_year,
+                year_high=query_year + grace_period,
+                patents=False,
+            )
+
+            results = list(itertools.islice(search_query, num_results))
+
+            print("Search URL: ", search_query._url)
+
+            return results, search_query._url
+        except Exception as e:
+            print("Trying different proxy!")
+            pg.get_next_proxy()
 
 
+force_update = True
+use_OR = False
 num_search_top_results = 200
 num_output_top_results = 10
+use_full_author_list = False  # Requires more google queries
+
+update_specific_venue = True
+update_venues = ["CVPR"]
+update_years = [2020]
+# update_years = list(range(2010, 2020))
 
 min_number_of_days_elapsed = int(365 / 2)
 
@@ -120,6 +158,8 @@ data_papers = yaml.load(open("../_data/papers.yml", "r"), Loader=Loader)
 data_venues = yaml.load(open("../_data/venues.yml", "r"), Loader=Loader)
 data_mostcited = yaml.load(open("../_data/mostcited.yml", "r"), Loader=Loader)
 
+random.shuffle(data_papers)
+
 today = date.today()
 today_str = today.strftime("%m/%d/%y")
 
@@ -134,6 +174,11 @@ for award in data_papers:
         print(f"Venue {venue} {year} was too recent: {elapsed_time.days} days.")
         continue
 
+    if update_specific_venue:
+        if venue not in update_venues or year not in update_years:
+            print(f"Venue {venue} {year} is not supposed to be updated.")
+            continue
+
     # ignore if the last update was today
     existing_entry = next(
         (
@@ -143,13 +188,13 @@ for award in data_papers:
         ),
         None,
     )
-    if existing_entry:
+    if existing_entry and not force_update:
         last_updated = datetime.strptime(
             existing_entry["last_updated"], "%m/%d/%y"
         ).date()
-        # if last_updated >= today:
-        #     print(f"Venue {venue} {year} was updated today. Skipping.")
-        #     continue
+        if last_updated >= today:
+            print(f"Venue {venue} {year} was updated today. Skipping.")
+            continue
 
     # get the proper search terms for google scholar
     search_terms = next((item for item in data_venues if item["name"] == venue), None)
@@ -162,15 +207,30 @@ for award in data_papers:
         f"Checking: {venue} {year}.  Search terms: {search_terms}".format(venue, year)
     )
 
-    google_search_query = " OR ".join(
-        ['source:"{}"'.format(search_term) for search_term in search_terms]
-    )
+    if use_OR:
+        google_search_query = " OR ".join(
+            ['source:"{}"'.format(search_term) for search_term in search_terms]
+        )
 
-    search_query = query_generator(google_search_query, venue, year)
-
-    print("Search URL: ", search_query._url)
-
-    results = list(itertools.islice(search_query, num_search_top_results))
+        results, search_query_url = query_generator(
+            google_search_query,
+            venue=venue,
+            year=year,
+            num_results=num_search_top_results,
+            pg=pg,
+        )
+    else:
+        results = []
+        for search_term in search_terms:
+            google_search_query = 'source:"{}"'.format(search_term)
+            results_tmp, search_query_url = query_generator(
+                google_search_query,
+                venue=venue,
+                year=year,
+                num_results=num_search_top_results,
+                pg=pg,
+            )
+            results.extend(results_tmp)
 
     # add best papers to search results
     # this requires a separate search query -- gscholar doesn't understand OR here (?)
@@ -181,14 +241,15 @@ for award in data_papers:
         if not any([paper["award"].startswith(x) for x in exclude_awards])
     ]
     for query in search_terms_best_papers:
-        best_paper_query = scholarly.search_pubs(
+        best_paper_results, _ = query_generator(
             '"{}"'.format(query),
-            year_low=year,
-            year_high=year
-            + 1,  # some grace period, some conference proceedings are published the next year
-            patents=False,
+            venue="",
+            year=year,
+            grace_period=1,  # some grace period, some conference proceedings are published the next year
+            num_results=1,
+            pg=pg,
         )
-        results.extend(list(itertools.islice(best_paper_query, 1)))
+        results.extend(best_paper_results)
 
     # sort by num citations
     sorted_results_tmp = sorted(results, key=lambda k: k["num_citations"], reverse=True)
@@ -198,12 +259,17 @@ for award in data_papers:
     new_entry_paper_titles = []
     for s in sorted_results_tmp:
         # avoid duplicates
-        if s["bib"]["title"] not in new_entry_paper_titles:
-            # filling is required to get all authors
-            new_s = scholarly.fill(s)
-
+        if s["bib"]["title"] not in new_entry_paper_titles and (
+            "eprint_url" in s or "pub_url" in s
+        ):
             new_entry_paper_titles.append(s["bib"]["title"])
-            sorted_results.append(new_s)
+
+            if use_full_author_list:
+                # filling is required to get all authors
+                new_s = scholarly.fill(s)
+                sorted_results.append(new_s)
+            else:
+                sorted_results.append(s)
 
             if len(sorted_results) >= num_output_top_results:
                 break
@@ -212,7 +278,7 @@ for award in data_papers:
     new_entry["venue"] = venue
     new_entry["year"] = year
     new_entry["last_updated"] = today_str
-    new_entry["source"] = "https://scholar.google.com{0}".format(search_query._url)
+    new_entry["source"] = "https://scholar.google.com{0}".format(search_query_url)
     new_entry["mostcited"] = []
     for s in sorted_results:
         print("Rank in search results:", results.index(s))
