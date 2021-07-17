c848e3eb5fc2771a8173fdda9a96ab7a1515dece
Clemens Eppner <ceppner@nvidia.com>
2021-07-17 10:58:32 -0700

    adding scripts

---
+++ utils/setup_tor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tor_password="myscholarly_password"
+hashed_password=$(tor --hash-password $tor_password)
+echo "ControlPort 9051" | sudo tee /etc/tor/torrc
+echo "HashedControlPassword $hashed_password" | sudo tee -a /etc/tor/torrc
+
+sudo service tor stop
+
+sudo service tor start
+
